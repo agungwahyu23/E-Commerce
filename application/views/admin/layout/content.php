872bb0fa7d90afd2f1f45c4e9369acb23f6262f2
agung wahyu <agungwahyu23699@gmail.com>
2020-10-21 22:58:15 +0700

    sesi 1-5

--- application/views/admin/layout/content.php
+++ application/views/admin/layout/content.php
@@ -1,3 +1,9 @@
+<section class="content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body">
 <?php
 //panggil; data isi content
 if ($isi) {
@@ -6,3 +12,10 @@ if ($isi) {
 }
 
 ?>
+
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- application/views/admin/layout/nav.php
+++ application/views/admin/layout/nav.php
@@ -1,11 +1,11 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= base_url() ?>assets/admin/index3.html" class="brand-link">
+    <a href="<?= base_url('admin/dasbor') ?>" class="brand-link">
       <img src="<?= base_url() ?>assets/admin/dist/img/AdminLTELogo.png"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light"><b>SWIPO</b></span>
     </a>
 
     <!-- Sidebar -->
@@ -63,7 +63,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>DataTables</h1>
+            <h1><?= $title ?></h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
