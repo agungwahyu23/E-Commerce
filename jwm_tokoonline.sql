fae07f9be0bbf21331cee2047e4c6b2e3e5a785d
IndyraAyuWijayanti <IndyraAyuWijayanti@github.com>
2020-11-22 12:03:21 +0700

    update database terbaru

--- jwm_tokoonline.sql
+++ jwm_tokoonline.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 16 Nov 2020 pada 13.02
--- Versi Server: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Nov 22, 2020 at 06:00 AM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `berita`
+-- Table structure for table `berita`
 --
 
 CREATE TABLE `berita` (
@@ -43,7 +45,7 @@ CREATE TABLE `berita` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `gambar`
+-- Table structure for table `gambar`
 --
 
 CREATE TABLE `gambar` (
@@ -57,7 +59,7 @@ CREATE TABLE `gambar` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `header_transaksi`
+-- Table structure for table `header_transaksi`
 --
 
 CREATE TABLE `header_transaksi` (
@@ -78,7 +80,7 @@ CREATE TABLE `header_transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Table structure for table `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -90,18 +92,18 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `slug_kategori`, `nama_kategori`, `urutan`, `tanggal_update`) VALUES
-(1, 'Buku', 'Buku', 1, '2020-11-13 17:52:41'),
-(2, 'video-tutorial-web', 'Video tutorial web', 2, '2020-11-13 18:19:08'),
-(4, 'ebook', 'eBook', 3, '2020-11-13 18:12:07');
+(5, 'sweater-pria', 'Sweater Pria', 1, '2020-11-17 12:52:02'),
+(6, 'sweater-wanita', 'Sweater Wanita', 2, '2020-11-17 12:52:11'),
+(7, 'jaket-denim', 'Jaket Denim', 3, '2020-11-17 12:52:19');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `konfigurasi`
+-- Table structure for table `konfigurasi`
 --
 
 CREATE TABLE `konfigurasi` (
@@ -123,10 +125,17 @@ CREATE TABLE `konfigurasi` (
   `tanggal_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `konfigurasi`
+--
+
+INSERT INTO `konfigurasi` (`id_konfigurasi`, `namaweb`, `tagline`, `email`, `website`, `keywords`, `metatext`, `telepon`, `alamat`, `facebook`, `instagram`, `deskripsi`, `logo`, `icon`, `rekening_pembayaran`, `tanggal_update`) VALUES
+(1, 'SWIPO Jember', NULL, 'swipojember@gmail.com', 'www.swipojember.com', 'ok', 'ok', '098766554444', 'Jember', 'swipo_jember', '@swipo', 'Website Untuk Reseller', 'ikon.png', 'ikon.png', 'ok', '2020-11-17 13:09:22');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -145,7 +154,7 @@ CREATE TABLE `pelanggan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
@@ -168,17 +177,23 @@ CREATE TABLE `produk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `produk`
+-- Dumping data for table `produk`
 --
 
 INSERT INTO `produk` (`id_produk`, `id_user`, `id_kategori`, `kode_produk`, `nama_produk`, `slug_produk`, `keterangan`, `keywords`, `harga`, `stok`, `gambar`, `berat`, `ukuran`, `status_produk`, `tanggal_post`, `tanggal_update`) VALUES
-(3, 8, 1, 'BOOK001', 'Buku Tulis', 'buku-tulis-book001', '<p>keternagan</p>\r\n', NULL, 10000, 10, 'ktm2.jpg', 10, '10x10', 'Publish', '2020-11-15 10:54:00', '2020-11-15 09:54:55'),
-(4, 8, 1, 'BOOK002', 'Buku Tulis halus', 'buku-tulis-halus-book002', '<p>halus</p>\r\n', NULL, 5000, 10, 'K02.jpg', 10, '10x10', 'Publish', '2020-11-15 11:01:00', '2020-11-15 10:01:00');
+(5, 8, 6, '01', 'Sweater Wanita Pink', 'sweater-wanita-pink-01', '<p>Sweater Pink</p>\r\n', NULL, 75000, 20, 'sww1.jpg', 750, 'M - XL', 'Publish', '2020-11-17 13:55:00', '2020-11-19 09:44:11'),
+(6, 8, 6, '02', 'Sweater Wanita Motif', 'sweater-wanita-motif-02', '<p>Sweater Motif</p>\r\n', NULL, 100000, 20, 'wn1.jpg', 500, 'M - XL', 'Publish', '2020-11-17 13:56:00', '2020-11-19 09:44:28'),
+(7, 8, 7, '03', 'Jaket Denim Biru', 'jaket-denim-biru-03', '<p>Denim Biru</p>\r\n', NULL, 150000, 10, 'dn1.jpg', 100, 'M - XL', 'Publish', '2020-11-17 13:57:00', '2020-11-19 09:44:38'),
+(8, 8, 7, '04', 'Jaket Denim Hitam', 'jaket-denim-hitam-04', '<p>Denim Hitam</p>\r\n', NULL, 150000, 20, 'ht1.jpg', 750, 'L - XL', 'Publish', '2020-11-17 13:57:00', '2020-11-19 09:45:45'),
+(9, 8, 7, '05', 'Jaket Denim Kuning', 'jaket-denim-kuning-05', '<p>Denim Kuning</p>\r\n', NULL, 150000, 20, 'kn1.jpg', 100, 'L - XL', 'Publish', '2020-11-17 13:58:00', '2020-11-19 09:45:59'),
+(10, 8, 5, '07', 'Sweater Puma', 'sweater-puma-07', '<p>Puma</p>\r\n', NULL, 200000, 20, 'pm.jpg', 150, 'L - XL', 'Publish', '2020-11-17 14:01:00', '2020-11-19 09:46:18'),
+(11, 8, 6, '06', 'Sweater Bunga', 'sweater-bunga-06', '<p>Bunga</p>\r\n', NULL, 130000, 20, 'bg.jpg', 60, 'L', 'Publish', '2020-11-17 14:02:00', '2020-11-19 09:46:30'),
+(12, 8, 5, '08', 'sweater Kotak', 'sweater-kotak-08', '<p>Kotak</p>\r\n', NULL, 200000, 15, 'ktk.jpg', 100, 'M - XL', 'Publish', '2020-11-17 14:04:00', '2020-11-19 09:46:43');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
 CREATE TABLE `transaksi` (
@@ -197,7 +212,7 @@ CREATE TABLE `transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -211,7 +226,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`, `akses_level`, `tanggal_update`) VALUES
@@ -288,46 +303,56 @@ ALTER TABLE `users`
 --
 ALTER TABLE `berita`
   MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `gambar`
 --
 ALTER TABLE `gambar`
   MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `header_transaksi`
 --
 ALTER TABLE `header_transaksi`
   MODIFY `id_header_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
-  MODIFY `id_konfigurasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_konfigurasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
