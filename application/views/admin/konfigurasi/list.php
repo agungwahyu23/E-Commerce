68eef6f24086c7284d68e26eb9077a13667faa39
Gulam Mubarik Ahmad <gulammubarik77@gmail.com>
2020-11-24 01:00:57 +0700

    1

--- application/views/admin/konfigurasi/list.php
+++ application/views/admin/konfigurasi/list.php
@@ -18,86 +18,93 @@ if ($this->session->flashdata('sukses')) {
 ?>
 
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Nama Website</label>
+	<label class="col-md-2 col-form-label control-label">Nama Website</label>
 	<div class="col-md-5">
 		<input type="text" name="namaweb" class="form-control" placeholder="Nama Website"
 			value="<?php echo $konfigurasi->namaweb ?>" required>
 	</div>
 </div>
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Tagline/Moto</label>
-	<div class="col-md-8">
-		<input type="text" name="tagline" class="form-control" placeholder="Tagline/Moto"
+	<label class="col-md-2 col-form-label control-label">Tagline/Moto</label>
+	<div class="col-md-5">
+	<input type="text" name="tagline" class="form-control" placeholder="Tagline/Moto"
 			value="<?php echo $konfigurasi->tagline ?>" required>
 	</div>
+</div>
 <div class="form-group row">
 	<label class="col-md-2 col-form-label control-label">Email</label>
-	<div class="col-md-8">
-		<input type="email" name="email" class="form-control" placeholder="Email"
+	<div class="col-md-5">
+	<input type="email" name="email" class="form-control" placeholder="Email"
 			value="<?php echo $konfigurasi->email ?>" required>
 	</div>
+</div>
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Website</label>
-	<div class="col-md-8">
-		<input type="text" name="website" class="form-control" placeholder="Website"
+	<label class="col-md-2 col-form-label control-label">Website</label>
+	<div class="col-md-5">
+	<input type="text" name="website" class="form-control" placeholder="Website"
 			value="<?php echo $konfigurasi->website ?>" required>
 	</div>
+</div>
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Alamat Facebook</label>
-	<div class="col-md-8">
+	<label class="col-md-2 col-form-label control-label">Alamat Facebook</label>
+	<div class="col-md-10">
 		<input type="text" name="facebook" class="form-control" placeholder="Facebook"
 			value="<?php echo $konfigurasi->facebook ?>" required>
 	</div>
+</div>
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Alamat Instagram</label>
-	<div class="col-md-8">
-		<input type="text" name="instagram" class="form-control" placeholder="Instagram"
+	<label class="col-md-2 col-form-label control-label">Alamat Instagram</label>
+	<div class="col-md-10">
+	<input type="text" name="instagram" class="form-control" placeholder="Instagram"
 			value="<?php echo $konfigurasi->instagram ?>" required>
 	</div>
+</div>
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Telepon/HP</label>
-	<div class="col-md-8">
-		<input type="text" name="telepon" class="form-control" placeholder="Telepon"
+	<label class="col-md-2 col-form-label control-label">Telepon/Hp</label>
+	<div class="col-md-10">
+	<input type="text" name="telepon" class="form-control" placeholder="Telepon"
 			value="<?php echo $konfigurasi->telepon ?>" required>
 	</div>
+</div>
 <div class="form-group row">
-	<label class="col-md-3 col-form-label control-label">Alamat Kantor</label>
-	<div class="col-md-8">
-		<input type="text" name="alamat" class="form-control" placeholder="Alamat Kantor"
+	<label class="col-md-2 col-form-label control-label">Alamat Kantor</label>
+	<div class="col-md-10">
+	<input type="text" name="alamat" class="form-control" placeholder="Alamat Kantor"
 			value="<?php echo $konfigurasi->alamat ?>" required>
 	</div>
 </div>
-<div class="form-group">
-	<label class="col-md-7 col-form-label control-label">Keywords (untuk SEO Google)</label>
-	<div class="col-md-15">
-		<textarea name="keywords" class="form-control" placeholder="keyword (untuk SEO Google)">
+<div class="form-group row">
+	<label class="col-md-2 col-form-label control-label">Keyword (Untuk SEO google)</label>
+	<div class="col-md-10">
+	<textarea name="keywords" class="form-control" placeholder="keyword (untuk SEO Google)">
 		<?php echo $konfigurasi->keywords ?></textarea>
 	</div>
 </div>
-<div class="form-group">
-	<label class="col-md-7 col-form-label control-label">Kode Metatext</label>
-	<div class="col-md-15">
-		<textarea name="metatext" class="form-control" placeholder="Metatext">
+<div class="form-group row">
+	<label class="col-md-2 col-form-label control-label">Kode Metatext</label>
+	<div class="col-md-10">
+	<textarea name="metatext" class="form-control" placeholder="Metatext">
 		<?php echo $konfigurasi->metatext ?></textarea>
 	</div>
 </div>
-<div class="form-group">
-	<label class="col-md-7 col-form-label control-label">Deskripsi</label>
-	<div class="col-md-15">
-		<textarea name="deskripsi" class="form-control" placeholder="Deskripsi Website">
+<div class="form-group row">
+	<label class="col-md-2 col-form-label control-label">Deskripsi</label>
+	<div class="col-md-10">
+	<textarea name="deskripsi" class="form-control" placeholder="Deskripsi Website">
 		<?php echo $konfigurasi->deskripsi ?></textarea>
 	</div>
 </div>
-<div class="form-group">
-	<label class="col-md-7 col-form-label control-label">Rekening Pembayran</label>
-	<div class="col-md-15">
-		<textarea name="rekening_pembayaran" class="form-control" placeholder="Rekening Pembayran">
+<div class="form-group row">
+	<label class="col-md-2 col-form-label control-label">Rekening Pembayaran</label>
+	<div class="col-md-10">
+	<textarea name="rekening_pembayaran" class="form-control" placeholder="Rekening Pembayran">
 		<?php echo $konfigurasi->rekening_pembayaran ?></textarea>
 	</div>
 </div>
+
 <div class="form-group row">
-	<label class="col-md-3 col-form-label"></label>
-	<div class="col-md-9">
+	<label class="col-md-2 col-form-label"></label>
+	<div class="col-md-5">
 		<button class="btn btn-success btn-lg" name="submit" type="submit">
 			<i class="fa fa-save"></i> Simpan
 		</button>
@@ -106,5 +113,7 @@ if ($this->session->flashdata('sukses')) {
 		</button>
 	</div>
 </div>
+
+
 					
 <?php echo form_close(); ?>
