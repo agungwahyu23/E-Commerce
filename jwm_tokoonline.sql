3c70591b7cbbf2f2237a658f307481b78f1463dc
agung wahyu <agungwahyu23699@gmail.com>
2020-12-02 01:32:01 +0700

    update database jwm_tokoonline.sql
    Silahkan databasenya diperbarui pake yang ini. Karena ada penambahan tabel rekening

--- jwm_tokoonline.sql
+++ jwm_tokoonline.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Nov 2020 pada 17.46
--- Versi server: 10.1.39-MariaDB
--- Versi PHP: 7.3.5
+-- Generation Time: 01 Des 2020 pada 19.30
+-- Versi Server: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -78,6 +76,9 @@ CREATE TABLE `header_transaksi` (
   `rekening_pembayaran` varchar(255) DEFAULT NULL,
   `rekening_pelanggan` varchar(255) DEFAULT NULL,
   `bukti_bayar` varchar(255) DEFAULT NULL,
+  `id_rekening` int(11) DEFAULT NULL,
+  `tanggal_bayar` varchar(150) DEFAULT NULL,
+  `nama_bank` varchar(150) DEFAULT NULL,
   `tanggal_post` datetime NOT NULL,
   `tanggal_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -86,8 +87,10 @@ CREATE TABLE `header_transaksi` (
 -- Dumping data untuk tabel `header_transaksi`
 --
 
-INSERT INTO `header_transaksi` (`id_header_transaksi`, `id_user`, `id_pelanggan`, `nama_pelanggan`, `email`, `telepon`, `alamat`, `kode_transaksi`, `tanggal_transaksi`, `jumlah_transaksi`, `status_bayar`, `jumlah_bayar`, `rekening_pembayaran`, `rekening_pelanggan`, `bukti_bayar`, `tanggal_post`, `tanggal_update`) VALUES
-(1, 0, 11, 'Andini asri', 'andiniasri@gmail.com', '087755334123', '                purwoharjo timur no 75, banyuwangi                ', '23112020VMGIRCO2', '2020-11-23 00:00:00', 250000, 'Belum', NULL, NULL, NULL, NULL, '2020-11-23 17:19:59', '2020-11-23 16:19:59');
+INSERT INTO `header_transaksi` (`id_header_transaksi`, `id_user`, `id_pelanggan`, `nama_pelanggan`, `email`, `telepon`, `alamat`, `kode_transaksi`, `tanggal_transaksi`, `jumlah_transaksi`, `status_bayar`, `jumlah_bayar`, `rekening_pembayaran`, `rekening_pelanggan`, `bukti_bayar`, `id_rekening`, `tanggal_bayar`, `nama_bank`, `tanggal_post`, `tanggal_update`) VALUES
+(4, 0, 13, 'pelanggan', 'pelanggan@yahoo.com', '0999111', '                Jl. Mawar 85 Jember    ', '30112020WUJ4YTAB', '2020-11-30 00:00:00', 200000, 'Konfirmasi', 200000, '23061999', 'Agung Wahyu Gunawan', 'images3.jpg', 1, '01-12-2020', 'BANK BRI SYARIAH', '2020-11-30 10:55:27', '2020-12-01 17:27:26'),
+(5, 0, 13, 'pelanggan', 'pelanggan@yahoo.com', '0999111', '                Jl. Mawar 85 Jember    ', '30112020BMAYJWT2', '2020-11-30 00:00:00', 200000, 'Belum', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-30 11:43:27', '2020-11-30 10:43:27'),
+(6, 0, 13, 'pelanggan', 'pelanggan@yahoo.com', '0999111', '                Jl. Mawar 85 Jember    ', '30112020PUEIK4CV', '2020-11-30 00:00:00', 150000, 'Belum', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-30 12:30:54', '2020-11-30 11:30:55');
 
 -- --------------------------------------------------------
 
@@ -168,7 +171,7 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `id_user`, `status_pelanggan`, `nama_pelanggan`, `email`, `password`, `telepon`, `alamat`, `tanggal_daftar`, `tanggal_update`) VALUES
-(11, 0, 'Pending', 'Andini asri', 'andiniasri@gmail.com', '841b6759d617721b2e00ae991a01f745b1367df7', '087755334123', 'purwoharjo timur no 75, banyuwangi                ', '2020-11-23 07:22:22', '2020-11-23 13:22:22');
+(13, 0, 'Pending', 'pelanggan agung wahyu', 'pelanggan@yahoo.com', 'e3e86d43002a494b7203dad168da9eb5e42dae14', '085816908859', '                            Jl. Mawar 85 Jember    ', '2020-12-01 12:30:21', '2020-12-01 12:30:21');
 
 -- --------------------------------------------------------
 
@@ -200,7 +203,7 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id_produk`, `id_user`, `id_kategori`, `kode_produk`, `nama_produk`, `slug_produk`, `keterangan`, `keywords`, `harga`, `stok`, `gambar`, `berat`, `ukuran`, `status_produk`, `tanggal_post`, `tanggal_update`) VALUES
-(5, 8, 6, '01', 'Sweater Wanita Pink', 'sweater-wanita-pink-01', '<p>Sweater Pink</p>\r\n', NULL, 75000, 20, 'sww1.jpg', 750, 'M - XL', 'Publish', '2020-11-17 13:55:00', '2020-11-19 09:44:11'),
+(5, 8, 6, '01', 'Sweater Wanita Pink ', '', '<p>Sweater Pink</p>\r\n', 'oke banget dah pokok		', 75000, 20, 'sww1.jpg', 75000, 'M - XL', 'Publish', '2020-11-17 13:55:00', '2020-12-01 18:20:37'),
 (6, 8, 6, '02', 'Sweater Wanita Motif', 'sweater-wanita-motif-02', '<p>Sweater Motif</p>\r\n', NULL, 100000, 20, 'wn1.jpg', 500, 'M - XL', 'Publish', '2020-11-17 13:56:00', '2020-11-19 09:44:28'),
 (7, 8, 7, '03', 'Jaket Denim Biru', 'jaket-denim-biru-03', '<p>Denim Biru</p>\r\n', NULL, 150000, 10, 'dn1.jpg', 100, 'M - XL', 'Publish', '2020-11-17 13:57:00', '2020-11-19 09:44:38'),
 (8, 8, 7, '04', 'Jaket Denim Hitam', 'jaket-denim-hitam-04', '<p>Denim Hitam</p>\r\n', NULL, 150000, 20, 'ht1.jpg', 750, 'L - XL', 'Publish', '2020-11-17 13:57:00', '2020-11-19 09:45:45'),
@@ -208,6 +211,28 @@ INSERT INTO `produk` (`id_produk`, `id_user`, `id_kategori`, `kode_produk`, `nam
 (10, 8, 5, '07', 'Sweater Puma', 'sweater-puma-07', '<p>Puma</p>\r\n', NULL, 200000, 20, 'pm.jpg', 150, 'L - XL', 'Publish', '2020-11-17 14:01:00', '2020-11-19 09:46:18'),
 (11, 8, 6, '06', 'Sweater Bunga', 'sweater-bunga-06', '<p>Bunga</p>\r\n', NULL, 130000, 20, 'bg.jpg', 60, 'L', 'Publish', '2020-11-17 14:02:00', '2020-11-19 09:46:30'),
 (12, 8, 5, '08', 'sweater Kotak', 'sweater-kotak-08', '<p>Kotak</p>\r\n', NULL, 200000, 15, 'ktk.jpg', 100, 'M - XL', 'Publish', '2020-11-17 14:04:00', '2020-11-19 09:46:43');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rekening`
+--
+
+CREATE TABLE `rekening` (
+  `id_rekening` int(11) NOT NULL,
+  `nama_bank` varchar(100) NOT NULL,
+  `nomor_rekening` varchar(20) NOT NULL,
+  `nama_pemilik` varchar(150) NOT NULL,
+  `gambar` varchar(100) DEFAULT NULL,
+  `tanggal_post` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `rekening`
+--
+
+INSERT INTO `rekening` (`id_rekening`, `nama_bank`, `nomor_rekening`, `nama_pemilik`, `gambar`, `tanggal_post`) VALUES
+(1, 'BANK BCA CABANG TANGGUL', '230699', 'AGUNG WAHYU', NULL, '2020-12-01 13:36:37');
 
 -- --------------------------------------------------------
 
@@ -233,8 +258,9 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id_transaksi`, `id_user`, `id_pelanggan`, `kode_transaksi`, `id_produk`, `harga`, `jumlah`, `total_harga`, `tanggal_transaksi`, `tanggal_update`) VALUES
-(1, 0, 11, '23112020VMGIRCO2', 6, 100000, 1, 100000, '2020-11-23 00:00:00', '2020-11-23 16:19:59'),
-(2, 0, 11, '23112020VMGIRCO2', 7, 150000, 1, 150000, '2020-11-23 00:00:00', '2020-11-23 16:19:59');
+(5, 0, 13, '30112020WUJ4YTAB', 10, 200000, 1, 200000, '2020-11-30 00:00:00', '2020-11-30 09:55:28'),
+(6, 0, 13, '30112020BMAYJWT2', 12, 200000, 1, 200000, '2020-11-30 00:00:00', '2020-11-30 10:43:27'),
+(7, 0, 13, '30112020PUEIK4CV', 8, 150000, 1, 150000, '2020-11-30 00:00:00', '2020-11-30 11:30:55');
 
 -- --------------------------------------------------------
 
@@ -264,122 +290,124 @@ INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`, `akses_
 --
 
 --
--- Indeks untuk tabel `berita`
+-- Indexes for table `berita`
 --
 ALTER TABLE `berita`
   ADD PRIMARY KEY (`id_berita`);
 
 --
--- Indeks untuk tabel `gambar`
+-- Indexes for table `gambar`
 --
 ALTER TABLE `gambar`
   ADD PRIMARY KEY (`id_gambar`);
 
 --
--- Indeks untuk tabel `header_transaksi`
+-- Indexes for table `header_transaksi`
 --
 ALTER TABLE `header_transaksi`
   ADD PRIMARY KEY (`id_header_transaksi`),
   ADD UNIQUE KEY `kode_transaksi` (`kode_transaksi`);
 
 --
--- Indeks untuk tabel `kategori`
+-- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indeks untuk tabel `konfigurasi`
+-- Indexes for table `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
   ADD PRIMARY KEY (`id_konfigurasi`);
 
 --
--- Indeks untuk tabel `pelanggan`
+-- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`),
   ADD UNIQUE KEY `kode_produk` (`kode_produk`);
 
 --
--- Indeks untuk tabel `transaksi`
+-- Indexes for table `rekening`
+--
+ALTER TABLE `rekening`
+  ADD PRIMARY KEY (`id_rekening`),
+  ADD UNIQUE KEY `nomor_rekening` (`nomor_rekening`);
+
+--
+-- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `berita`
+-- AUTO_INCREMENT for table `berita`
 --
 ALTER TABLE `berita`
   MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT;
-
 --
--- AUTO_INCREMENT untuk tabel `gambar`
+-- AUTO_INCREMENT for table `gambar`
 --
 ALTER TABLE `gambar`
   MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT;
-
 --
--- AUTO_INCREMENT untuk tabel `header_transaksi`
+-- AUTO_INCREMENT for table `header_transaksi`
 --
 ALTER TABLE `header_transaksi`
-  MODIFY `id_header_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `id_header_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT untuk tabel `kategori`
+-- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
 --
--- AUTO_INCREMENT untuk tabel `konfigurasi`
+-- AUTO_INCREMENT for table `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
   MODIFY `id_konfigurasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
--- AUTO_INCREMENT untuk tabel `pelanggan`
+-- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
 --
--- AUTO_INCREMENT untuk tabel `transaksi`
+-- AUTO_INCREMENT for table `rekening`
+--
+ALTER TABLE `rekening`
+  MODIFY `id_rekening` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
